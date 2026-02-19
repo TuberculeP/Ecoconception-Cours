7695e6d355a6b8919b1cb42343a37589a1c6f5f3
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2026-02-19 12:00:38 +0100

    feat: basic sentry config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,5 @@
+import "./config/sentry.config";
+import Sentry from "@sentry/node";
 import "dotenv/config";
 import pg from "./config/db.config";
 import path from "path";
@@ -104,6 +106,7 @@ ${articles
     }
   });
 
+  Sentry.setupExpressErrorHandler(app);
   app.listen(3000, () => {
     console.log("> Ready on http://localhost:3000");
   });
